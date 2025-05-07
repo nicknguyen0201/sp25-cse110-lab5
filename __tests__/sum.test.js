d650af1870c2512499816ca740c28b177d5b8833
Nick Nguyen <nin002@ucsd.edu>
2025-05-06 21:42:52 -0700

    Update sum.test.js again to pass

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,5 +3,5 @@ import { sum } from '../code-to-unit-test/sum';
 test('adds 1 + 2 to equal 3', () => {
   // TODO
   //intentionally set toBe(5) to prevent push to main
-  expect(sum(1,2)).toBe(5);
+  expect(sum(1,2)).toBe(3);
 });
